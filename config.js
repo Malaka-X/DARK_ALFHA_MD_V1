d28c0870322ad78745d4a6c2ae993ff64b84c2b6
Malaka-X <malakamdbot@gmail.com>
2024-12-10 14:40:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "youre session id",
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "GS4w2Kyb#PglZdhL-rMUZpO4snXWRak3Mn_CaGLXyswtBhN-BqSg",
+MONGODB: process.env.MONGODB || "mongodb://mongo:PIDRqpahCzzZlpwYFWqShxAURtcFoRje@autorack.proxy.rlwy.net:32217",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/QNwLWTN/20241201-230018.jpg",
 BOT_NAME: process.env.BOT_NAME || "➺ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ࿐",
 LANG: process.env.BOT_LANG || 'EN' ,
